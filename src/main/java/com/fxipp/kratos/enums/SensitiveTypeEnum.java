410cef9c9f6234963c95e5061301ff99d81d2358
fangxi <512618664@qq.com>
2019-11-13 22:15:22 +0800

    脱敏

---
+++ src/main/java/com/fxipp/kratos/enums/SensitiveTypeEnum.java
@@ -0,0 +1,36 @@
+package com.fxipp.kratos.enums;
+
+/**
+ * @author fangxi
+ */
+public enum SensitiveTypeEnum {
+
+
+    /**
+     * 身份证号
+     */
+    ID_CARD,
+    /**
+     * 密码
+     */
+    PASSWORD,
+    /**
+     * 手机号
+     */
+    MOBILE_PHONE,
+    /**
+     * 电子邮件
+     */
+    EMAIL,
+    /**
+     * 真实姓名
+     */
+    NAME,
+    /**
+     * 账户信息
+     */
+    ACCOUNT_NO;
+
+
+
+}
