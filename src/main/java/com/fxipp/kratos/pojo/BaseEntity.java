c68ebc3a853a18c21fc834b0c59baee56babcc29
fangxi <loveinori@qq.com>
2020-05-26 11:23:39 +0800

    BaseEntity 添加 1.3.9

---
+++ src/main/java/com/fxipp/kratos/pojo/BaseEntity.java
@@ -0,0 +1,41 @@
+package com.fxipp.kratos.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author fangxi
+ */
+@Data
+@Accessors(chain = true)
+public class BaseEntity {
+
+    /**
+     * 主键id
+     */
+    @TableId(value = "id", type = IdType.ASSIGN_ID)
+    private Long id;
+
+    /**
+     * 创建时间 默认当前时间
+     */
+    private LocalDateTime createTime;
+
+    /**
+     * 更新时间 默认当前时间
+     */
+    private LocalDateTime updateTime;
+
+    /**
+     * 删除标识位 默认不删除
+     */
+    @TableField(value = "is_deleted")
+    @TableLogic
+    private Integer deleted;
+}
