929cd0776925f87eb3ceabfa7b807b3551cf93f9
fangxi <loveinori@qq.com>
2020-06-30 20:59:09 +0800

    1.4.5, CollectionUtils修改

--- src/main/java/com/fxipp/kratos/utils/CollectionUtils.java
+++ src/main/java/com/fxipp/kratos/utils/CollectionUtils.java
@@ -1,9 +1,8 @@
 package com.fxipp.kratos.utils;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import org.springframework.lang.Nullable;
+
+import java.util.*;
 
 /**
  * @author fangxi
@@ -21,5 +20,14 @@ public class CollectionUtils extends org.springframework.util.CollectionUtils {
     public static <K, V> Map<K,V> nonNull(Map<K, V> map) {
         return isEmpty(map) ? Collections.emptyMap() : map;
     }
+
+    public static boolean isNotEmpty(@Nullable Collection<?> collection) {
+        return !isEmpty(collection);
+    }
+
+    public static boolean isNotEmpty(@Nullable Map<?, ?> map) {
+        return !isEmpty(map);
+    }
+
 
 }
