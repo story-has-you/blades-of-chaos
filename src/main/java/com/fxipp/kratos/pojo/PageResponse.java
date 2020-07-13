ed797c6e5095b779c25e4ec506018d4952919bef
fangxi <loveinori@qq.com>
2020-07-13 16:48:09 +0800

    PageResponse

--- src/main/java/com/fxipp/kratos/pojo/PageResponse.java
+++ src/main/java/com/fxipp/kratos/pojo/PageResponse.java
@@ -1,7 +1,5 @@
 package com.fxipp.kratos.pojo;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.fxipp.kratos.utils.BeanUtils;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -38,31 +36,6 @@ public class PageResponse<T> {
      */
     @NonNull
     private Long size;
-
-
-    public static PageResponse<?> of(IPage<?> iPage) {
-        List<?> records = iPage.getRecords();
-        long total = iPage.getTotal();
-        long pages = iPage.getPages();
-        long size = iPage.getSize();
-        return PageResponse.of(records, total, pages, size);
-    }
-
-    public static <T> PageResponse<T> of(IPage<?> iPage, Class<T> target) {
-        List<?> records = iPage.getRecords();
-        long total = iPage.getTotal();
-        long pages = iPage.getPages();
-        long size = iPage.getSize();
-        List<T> rows = BeanUtils.copyProperties(records, target);
-        return PageResponse.of(rows, total, pages, size);
-    }
-
-    public static <T> PageResponse<T> of(IPage<?> iPage, List<T> rows) {
-        long total = iPage.getTotal();
-        long pages = iPage.getPages();
-        long size = iPage.getSize();
-        return PageResponse.of(rows, total, pages, size);
-    }
 
 }
 
