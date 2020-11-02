771472caa6953533951f392a6d2d928f4d2cf941
方曦 <fangxisama@dingtalk.com>
2020-11-02 11:02:28 +0800

    添加一些配置

--- src/main/java/com/storyhasyou/kratos/config/BladesOfChaosConfig.java
+++ src/main/java/com/storyhasyou/kratos/config/BladesOfChaosConfig.java
@@ -4,9 +4,11 @@ import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.storyhasyou.kratos.exceptions.BusinessExceptionHandler;
 import com.storyhasyou.kratos.repository.DefaultValueMetaObjectHandler;
 import com.storyhasyou.kratos.utils.JsonUtils;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
@@ -29,19 +31,34 @@ public class BladesOfChaosConfig {
     }
 
     /**
+     * Exception handler business exception handler.
+     *
+     * @return the business exception handler
+     */
+    @Bean
+    public BusinessExceptionHandler exceptionHandler() {
+        return new BusinessExceptionHandler();
+    }
+
+
+    /**
      * Mapping jackson 2 http message converter mapping jackson 2 http message converter.
      *
      * @return the mapping jackson 2 http message converter
      */
     @Bean
+    @ConditionalOnMissingBean(MappingJackson2HttpMessageConverter.class)
     public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
         return JsonUtils.mappingJackson2HttpMessageConverter(PropertyNamingStrategy.SNAKE_CASE);
     }
 
     /**
      * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+     *
+     * @return the mybatis plus interceptor
      */
     @Bean
+    @ConditionalOnMissingBean(MybatisPlusInterceptor.class)
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
@@ -50,5 +67,6 @@ public class BladesOfChaosConfig {
         interceptor.addInnerInterceptor(paginationInnerInterceptor);
         return interceptor;
     }
+
 
 }
