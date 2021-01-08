0818773c2d2fe1caeafc73e6e1c2466da1fe4f80
fangxi <loveinori@qq.com>
2021-01-08 20:54:53 +0800

    2.2.0

--- src/main/java/com/storyhasyou/kratos/handler/HttpRequestFilter.java
+++ src/main/java/com/storyhasyou/kratos/handler/HttpRequestFilter.java
@@ -2,17 +2,18 @@ package com.storyhasyou.kratos.handler;
 
 import com.storyhasyou.kratos.utils.IdUtils;
 import com.storyhasyou.kratos.utils.TraceIdUtils;
-import java.io.IOException;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.StopWatch;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author 方曦 created by 2021/1/6
@@ -44,7 +45,8 @@ public class HttpRequestFilter extends OncePerRequestFilter {
         } finally {
             stopWatch.stop();
             if (log.isDebugEnabled()) {
-                if (!StringUtils.equals(IGNORE_CONTENT_TYPE, request.getContentType())) {
+                if (!StringUtils.equals(IGNORE_CONTENT_TYPE, request.getContentType())
+                        && request.getRequestURI().startsWith("/api")) {
                     String requestBody = new String(requestWrapper.getContentAsByteArray());
                     String responseBody = new String(responseWrapper.getContentAsByteArray());
                     log.debug("traceId:{}, requestBody:{}, responseBody:{}, totalTimeMillis:{}", traceId,
