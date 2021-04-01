09e322177716a263e603d60b50a8a27c86eac148
fangxi <loveinori@qq.com>
2021-04-01 17:39:28 +0800

    emmm

--- src/main/java/com/storyhasyou/kratos/annotation/EnableBladesOfChaos.java
+++ src/main/java/com/storyhasyou/kratos/annotation/EnableBladesOfChaos.java
@@ -1,15 +1,15 @@
 package com.storyhasyou.kratos.annotation;
 
 import com.storyhasyou.kratos.config.BladesOfChaosConfig;
-import com.storyhasyou.kratos.config.CorsConfig;
 import com.storyhasyou.kratos.config.ThreadPoolConfiguration;
 import com.storyhasyou.kratos.utils.SpringUtils;
+import org.springframework.context.annotation.Import;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import org.springframework.context.annotation.Import;
 
 /**
  * @author fangxi created by 2020/11/1
@@ -17,6 +17,6 @@ import org.springframework.context.annotation.Import;
 @Documented
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@Import({BladesOfChaosConfig.class, ThreadPoolConfiguration.class, SpringUtils.class, CorsConfig.class})
+@Import({BladesOfChaosConfig.class, ThreadPoolConfiguration.class, SpringUtils.class})
 public @interface EnableBladesOfChaos {
 }
